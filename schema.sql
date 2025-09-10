87a748507da0ea8e19c1907c3c7f973d51d4d478
Jonathan Kee <48512757+jonathan-kee@users.noreply.github.com>
2025-09-10 22:36:08 +0800

    initial commit

---
+++ schema.sql
@@ -0,0 +1,50 @@
+-- As a general rule, all the columns in a unique constraint and all the columns in a foreign key constraint are best defined with not null constraints as well; this will often be a business requirement. Attempting to insert a row in the child table for which there is no matching row in the parent table will give an error --
+
+CREATE TABLE companies (
+    name STRING,
+    CONSTRAINT company_pk PRIMARY KEY (name)
+)
+
+INSERT INTO companies (name) 
+VALUES ('Oracle');
+
+CREATE TABLE exams (
+    name STRING,
+    company STRING NOT NULL,
+    CONSTRAINT exam_pk PRIMARY KEY (name),
+    CONSTRAINT exam_company_fk FOREIGN KEY (company) REFERENCES companies(name) ON DELETE SET NULL
+);
+INSERT INTO exams (name, company) 
+VALUES ('1z0-071', 'Oracle')
+
+CREATE TABLE questions (
+    number INT,
+    exam STRING NOT NULL,
+    text STRING,
+    -- Each question belongs to exactly one exam, but an exam can have many questions. The combination of question number and exam must be unique. --
+    -- composite primary key --
+    CONSTRAINT question_pk PRIMARY KEY (number, exam),
+    CONSTRAINT question_exam_fk FOREIGN KEY (exam) REFERENCES exams(name) ON DELETE SET NULL
+);
+
+INSERT INTO questions (number, exam, text) 
+VALUES (1, '1z0-071', 'Which SQL statement is used to extract data from a database?');
+
+CREATE TABLE answers (
+    number INT,
+    question_number INT NOT NULL,
+    question_exam STRING NOT NULL,
+    text STRING,
+    is_correct BOOL,
+    -- Each answer belongs to exactly one question, but a question can have many answers. The combination of answer number and question (number and exam) must be unique. --
+    -- composite primary key --
+    CONSTRAINT answer_pk PRIMARY KEY (number, question_number, question_exam),
+    CONSTRAINT answer_question_fk  FOREIGN KEY (question_number, question_exam) REFERENCES questions(number, exam) ON DELETE SET NULL
+)
+
+INSERT INTO answers (number, question_number, question_exam, text, is_correct) 
+VALUES (1, 1, '1z0-071', 'SELECT', TRUE),
+       (2, 1, '1z0-071', 'EXTRACT', FALSE),
+       (3, 1, '1z0-071', 'OPEN', FALSE),
+       (4, 1, '1z0-071', 'GET', FALSE);
+
