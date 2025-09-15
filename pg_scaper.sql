294a1048964c6dcbe4b115ed7cf254e0d17a8484
Jonathan Kee <48512757+jonathan-kee@users.noreply.github.com>
2025-09-15 23:52:44 +0800

    questions link schema for postgres

---
+++ pg_scaper.sql
@@ -0,0 +1,11 @@
+CREATE TABLE questionsLink (
+    number INT,
+    exam TEXT NOT NULL,
+    text TEXT,
+    CONSTRAINT question_pk PRIMARY KEY (number, exam)
+);
+
+CREATE SEQUENCE seq_questionsLink START WITH 0 INCREMENT BY 1;
+
+INSERT INTO questionsLink (number, exam, text) 
+VALUES (nextval('seq_questionsLink'), '1z0-071', 'https://www.examtopics.com/discussions/oracle/view/79888-exam-1z0-071-topic-1-question-1-discussion/');
