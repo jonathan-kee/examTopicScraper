4cc9012718840bf5435b03e6b57c06ebb47d5e38
Jonathan Kee <48512757+jonathan-kee@users.noreply.github.com>
2025-09-16 20:18:47 +0800

    postgres schema

---
+++ pg_schema.sql
@@ -0,0 +1,70 @@
+CREATE TABLE companies
+(
+    name TEXT,
+    CONSTRAINT company_pk PRIMARY KEY (name)
+);
+
+INSERT INTO companies (name) VALUES ('Oracle');
+
+CREATE TABLE exams
+(
+    name TEXT,
+    company TEXT NOT NULL,
+    CONSTRAINT exam_pk PRIMARY KEY (name),
+    CONSTRAINT exam_company_fk FOREIGN KEY (company) REFERENCES companies(name)
+);
+
+INSERT INTO exams (name, company) VALUES ('1z0-071', 'Oracle');
+
+CREATE TABLE questions
+(
+    number INT,
+    exam TEXT NOT NULL,
+    text TEXT,
+    CONSTRAINT question_pk PRIMARY KEY (number, exam),
+    CONSTRAINT question_exam_fk FOREIGN KEY (exam) REFERENCES exams(name)
+);
+
+INSERT INTO questions (number, exam, text)
+VALUES (1, '1z0-071', 'Which SQL statement is used to extract data from a database?');
+
+CREATE TABLE answers
+(
+    number INT,
+    question_number INT NOT NULL,
+    question_exam TEXT NOT NULL,
+    text TEXT,
+    is_correct BOOLEAN,
+    CONSTRAINT answer_pk PRIMARY KEY (number, question_number, question_exam),
+    CONSTRAINT answer_question_fk FOREIGN KEY (question_number, question_exam)
+        REFERENCES questions(number, exam)
+);
+
+INSERT INTO answers (number, question_number, question_exam, text, is_correct)
+VALUES
+    (1, 1, '1z0-071', 'SELECT', true),
+    (2, 1, '1z0-071', 'EXTRACT', false),
+    (3, 1, '1z0-071', 'OPEN', false),
+    (4, 1, '1z0-071', 'GET', false);
+
+CREATE TABLE discussions
+(
+    number INT,
+    question_number INT NOT NULL,
+    question_exam TEXT NOT NULL,
+    selected_answer TEXT,
+    text TEXT,
+    upvote INT,
+    CONSTRAINT discussion_pk PRIMARY KEY (number, question_number, question_exam),
+    CONSTRAINT discussion_question_fk FOREIGN KEY (question_number, question_exam)
+        REFERENCES questions(number, exam)
+);
+
+INSERT INTO discussions (number, question_number, question_exam, selected_answer, text, upvote)
+VALUES
+    (1, 1, '1z0-071', 'Selected Answer: AC', 'A & C is correct', 1),
+    (2, 1, '1z0-071', 'Selected Answer: AC', 'A-C is correct', 1),
+    (3, 1, '1z0-071', null, 'A and C is the correct answer.', 1),
+    (4, 1, '1z0-071', 'Selected Answer: AC',
+        'Distinct is used to get distinct set of values for one or more columns mentioned in select statement',
+        2);
