ef34c1c2dcb0fc7afac9827a500374d1bd813f5d
Jonathan Kee <48512757+jonathan-kee@users.noreply.github.com>
2025-09-20 16:04:51 +0800

    sql code to convert to relative path

---
+++ cleanImages.sql
@@ -0,0 +1,31 @@
+CREATE TABLE relative_path_questions AS
+with relative_path_questions as (
+    SELECT 
+    number,
+    exam,
+    regexp_replace(
+        text,
+        'https://www\.examtopics\.com/[^\s'']+/([0-9]+\.png)',
+        './images/\1',
+        'g'
+    ) as text
+    from questions
+)
+select * from relative_path_questions;
+
+CREATE TABLE relative_path_answers AS
+with relative_path_answers as (
+    SELECT 
+    number,
+    question_number,
+    question_exam,
+    regexp_replace(
+        text,
+        'https://www\.examtopics\.com/[^\s'']+/([0-9]+\.png)',
+        './images/\1',
+        'g'
+    ) as text,
+    is_correct
+    from answers
+)
+select * from relative_path_answers;
