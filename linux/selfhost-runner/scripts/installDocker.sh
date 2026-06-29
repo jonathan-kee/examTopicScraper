b27f287752292733fd75794022eb7251fdd327f3
jonathan kee <jonathankee99@gmail.com>
2026-06-30 03:58:14 +0800

    bug fix, allow github runner to have docker permission

--- linux/selfhost-runner/scripts/installDocker.sh
+++ linux/selfhost-runner/scripts/installDocker.sh
@@ -6,3 +6,12 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl --now enable docker
+
+# Add the current user
+sudo usermod -a -G docker $(whoami)
+
+# Apply the group changes:
+newgrp docker
+
+# Restart the Docker service (if needed):
+sudo systemctl restart docker
