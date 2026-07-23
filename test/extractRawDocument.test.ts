de00b9d628b1b6a9025d6ab64198cfdc73540a99
jonathan kee <jonathankee99@gmail.com>
2026-07-23 10:54:27 +0800

    update code to download more document

--- test/extractRawDocument.test.ts
+++ test/extractRawDocument.test.ts
@@ -1,18 +1,22 @@
 import t from 'tap'
 import fs from "fs";
 
-async function captureAsset(url: string) {
+async function captureAsset(number: number, url: string,) {
   // Fetch HTML/CSS/JS from target server
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
   }
   const data = await response.arrayBuffer(); // Get raw binary or text buffer
-  fs.writeFileSync("./document/" + "TestDocument", Buffer.from(data));
+  fs.writeFileSync("./document/" + "Document" + number, Buffer.from(data));
 
-  console.log("Document saved");
+  console.log("Document " + number + " saved");
 }
 
 t.test('test capture document from page', async t => {
-    await captureAsset("https://www.examtopics.com/discussions/oracle/view/79888-exam-1z0-071-topic-1-question-1-discussion/");
+    await captureAsset(1,"https://www.examtopics.com/discussions/oracle/view/79888-exam-1z0-071-topic-1-question-1-discussion/");
+    await captureAsset(2,"https://www.examtopics.com/discussions/oracle/view/79530-exam-1z0-071-topic-1-question-2-discussion/"); 
+    await captureAsset(3,"https://www.examtopics.com/discussions/oracle/view/82940-exam-1z0-071-topic-1-question-3-discussion/");
+    await captureAsset(4,"https://www.examtopics.com/discussions/oracle/view/79642-exam-1z0-071-topic-1-question-4-discussion/");
+    await captureAsset(5,"https://www.examtopics.com/discussions/oracle/view/79891-exam-1z0-071-topic-1-question-5-discussion/");
 })
